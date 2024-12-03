cc226f5edb0e110dda79b580322ce04c516af9d6
ShowMak1rr <showmaker467@gmail.com>
2024-12-04 01:45:58 +0800

    Big D

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -7,7 +7,7 @@ export const getPosts = () => async (dispatch) => {
     }   catch (error) {
         console.log(error.message)
     }
-};
+}
 
 export const createPost = (post) => async (dispatch) => {
     try {
@@ -15,5 +15,41 @@ export const createPost = (post) => async (dispatch) => {
         dispatch({type: 'CREATE', payload: data});
     }   catch(error) {
         console.log(error);
+    }
+}
+
+
+export const updatePost = (id, post) => async (dispatch) => {
+    try {
+      const { data } = await api.updatePost(id, post);
+  
+      dispatch({ type: 'UPDATE', payload: data });
+    } catch (error) {
+      console.log(error);
+    }
+}
+
+
+
+  
+export const deletePost = (id) => async (dispatch) => {
+    try {
+      await api.deletePost(id);
+  
+      dispatch({ type: 'DELETE', payload: id });
+    } catch (error) {
+      console.log(error);
+    }
+}
+  
+
+
+export const likePost = (id) => async (dispatch) => {
+    try {
+        const { data } = await api.likePost(id);
+  
+        dispatch({ type: 'UPDATA', payload: data });
+    } catch (error) {
+      console.log(error);
     }
 }
